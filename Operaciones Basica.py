4e48c33b1a8c8d722155c46e9b4da4f7bc5fcc3d
gabrielaamaya <amayagabriela073@gmail.com>
2025-08-20 14:15:48 -0600

    Tarea de la Operaciones Basicas 29/07/2025

---
+++ Operaciones Basica.py
@@ -0,0 +1,15 @@
+#entrada
+name = "Gabriela"
+numeros01 = 5
+numeros02 = 2
+#procesos
+suma = (numeros01+numeros02)
+resta = (numeros01-numeros02)
+multi = (numeros01*numeros02)
+divi = (numeros01/numeros02)
+#salida
+print("Hecho por: "+ name + "\n")
+print("Suma ="+ str (suma))
+print("Resta = "+ str (resta))
+print("Multi = "+ str (multi))
+print("Division = "+ str (divi))
