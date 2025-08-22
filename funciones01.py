3aad18de4d8547d00c9dcbd750e4ec6e823d93d0
gabrielaamaya <amayagabriela073@gmail.com>
2025-08-22 09:49:53 -0600

    ejemplo de funciones de los numeros al cuadrado

---
+++ funciones01.py
@@ -0,0 +1,10 @@
+#crear la funcion
+def cuadrado_numero(num1):
+    return num1 ** 2
+
+#solicita la variable
+num1 = int (input("introduce el numero para calcular: "))
+
+
+#imprimir el resultado
+print(f"El cuadrado de: {num1} es:{cuadrado_numero(num1)}")
