61a5d8b1bea17e954100eac26ce13a095c64cd55
gabrielaamaya <amayagabriela073@gmail.com>
2025-08-22 09:57:18 -0600

    funciones de suma

--- funciones01.py
+++ funciones01.py
@@ -1,10 +1,10 @@
-#crear la funcion
-def cuadrado_numero(num1):
-    return num1 ** 2
+# crear función
+def suma_numeros(num1, num2):
+    return num1 + num2
 
-#solicita la variable
-num1 = int (input("introduce el numero para calcular: "))
+# solicitar variables
+num1 = int(input("Introduzca el primer número: "))
+num2 = int(input("Introduzca el segundo número: "))
 
-
-#imprimir el resultado
-print(f"El cuadrado de: {num1} es:{cuadrado_numero(num1)}")
+# imprimir el resultado
+print(f"La suma de {num1} + {num2} es {suma_numeros(num1, num2)}")
