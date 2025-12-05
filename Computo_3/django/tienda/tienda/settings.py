a1a993e8a67d32c5463bf274cb9aa525b622403a
gabrielaamaya <amayagabriela073@gmail.com>
2025-12-05 16:36:14 -0600

    Practic 5 -- Tarea -- 05/12/2025

--- Computo_3/django/tienda/tienda/settings.py
+++ Computo_3/django/tienda/tienda/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'articulosclientes',
+      'USER': 'postgres',
+      'PASSWORD': '123456',
+      'HOST': '127.0.0.1',
+      'PORT': '5432',
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
